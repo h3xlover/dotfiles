21f4b2c47bff8b4903ff011013096d4e9c9a8690
gcorreiaq <gabrielcqueiroz@protonmail.com>
2023-04-22 08:57:44 -0300

    update emacs config

--- emacs/init.el
+++ emacs/init.el
@@ -4,11 +4,11 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(custom-enabled-themes '(darcula))
  '(custom-safe-themes
    '("79586dc4eb374231af28bbc36ba0880ed8e270249b07f814b0e6555bdcb71fab" default))
+ '(org-agenda-files '("/home/g/org/tasks/tasks.org"))
  '(package-selected-packages
-   '(swiper ivy ace-window avy ido-vertical-mode nov exwm which-key darcula-theme use-package)))
+   '(elfeed-org org-bullets org-ql tron-legacy-theme swiper ivy ace-window avy ido-vertical-mode nov exwm which-key darcula-theme use-package)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
