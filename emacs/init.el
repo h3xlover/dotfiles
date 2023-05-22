e6a090bd19a57fc302b5615bce3186614f98fa11
gcorreiaq <gabrielcqueiroz@protonmail.com>
2023-05-21 22:23:12 -0300

    a

--- emacs/init.el
+++ emacs/init.el
@@ -8,13 +8,15 @@
    '("79586dc4eb374231af28bbc36ba0880ed8e270249b07f814b0e6555bdcb71fab" default))
  '(org-agenda-files '("/home/g/org/tasks/tasks.org"))
  '(package-selected-packages
-   '(latex-preview-pane elfeed-org org-bullets org-ql tron-legacy-theme swiper ivy ace-window avy ido-vertical-mode nov exwm which-key darcula-theme use-package)))
+   '(org-download org-roam-ui org-roam latex-preview-pane elfeed-org org-bullets org-ql tron-legacy-theme swiper ivy ace-window avy ido-vertical-mode nov exwm which-key darcula-theme use-package))
+ '(warning-suppress-log-types '((websocket))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
+
 
 (eval-when-compile
     ;; Following line is not needed if use-package.el is in ~/.emacs.d
