1f444ea883165fe2855a0d3f93af89b73839cf89
gcorreiaq <gabrielcqueiroz@protonmail.com>
2024-07-18 16:20:33 -0300

    recompiled all packages

--- emacs/.emacs.d/elpa/org-roam-ui-20221105.1040/org-roam-ui-autoloads.el
+++ emacs/.emacs.d/elpa/org-roam-ui-20221105.1040/org-roam-ui-autoloads.el
@@ -25,18 +25,18 @@ Enable org-roam-ui.
 This serves the web-build and API over HTTP.
 
 This is a global minor mode.  If called interactively, toggle the
-`org-roam-ui mode' mode.  If the prefix argument is positive,
-enable the mode, and if it is zero or negative, disable the mode.
+`org-roam-ui mode' mode.  If the prefix argument is positive, enable the
+mode, and if it is zero or negative, disable the mode.
 
-If called from Lisp, toggle the mode if ARG is `toggle'.  Enable
-the mode if ARG is nil, omitted, or is a positive number.
-Disable the mode if ARG is a negative number.
+If called from Lisp, toggle the mode if ARG is `toggle'.  Enable the
+mode if ARG is nil, omitted, or is a positive number.  Disable the mode
+if ARG is a negative number.
 
 To check whether the minor mode is enabled in the current buffer,
 evaluate `(default-value \\='org-roam-ui-mode)'.
 
-The mode's hook is called both when the mode is enabled and when
-it is disabled.
+The mode's hook is called both when the mode is enabled and when it is
+disabled.
 
 (fn &optional ARG)" t)
 (autoload 'org-roam-ui-open "org-roam-ui" "\
@@ -77,19 +77,18 @@ or call the function `org-roam-ui-follow-mode'.")
 Set whether ORUI should follow your every move in Emacs.
 
 This is a global minor mode.  If called interactively, toggle the
-`org-roam-ui-Follow mode' mode.  If the prefix argument is
-positive, enable the mode, and if it is zero or negative, disable
-the mode.
+`org-roam-ui-Follow mode' mode.  If the prefix argument is positive,
+enable the mode, and if it is zero or negative, disable the mode.
 
-If called from Lisp, toggle the mode if ARG is `toggle'.  Enable
-the mode if ARG is nil, omitted, or is a positive number.
-Disable the mode if ARG is a negative number.
+If called from Lisp, toggle the mode if ARG is `toggle'.  Enable the
+mode if ARG is nil, omitted, or is a positive number.  Disable the mode
+if ARG is a negative number.
 
 To check whether the minor mode is enabled in the current buffer,
 evaluate `(default-value \\='org-roam-ui-follow-mode)'.
 
-The mode's hook is called both when the mode is enabled and when
-it is disabled.
+The mode's hook is called both when the mode is enabled and when it is
+disabled.
 
 (fn &optional ARG)" t)
 (register-definition-prefixes "org-roam-ui" '("img/:file" "node/:id" "org-roam-ui-"))
